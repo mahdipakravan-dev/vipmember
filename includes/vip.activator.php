612bc4695b83b431aee13f020e70e03f9b1a1065
mahdipakravan-dev <mahdipakravan79@gmail.com>
2023-09-14 14:02:58 +0430

    feat : admin menus

--- includes/vip.activator.php
+++ includes/vip.activator.php
@@ -1,10 +1,20 @@
 <?php
 
-require_once VIP_DIR . "includes/vip.db-initializer.php";
-class VipActivator {
+require_once VIP_DIR . "inc/helpers/exchange-helper.php";
+class Activator {
 	public static function activate() {
-        $dbInitializer = new VipDBInitializer();
-        $dbInitializer->initialize();
+        $table_name = VipDBHelper::get_table_name(VipDBHelper::VIP_PLANS_TABLE);
+        VipDBHelper::create_table("CREATE TABLE $table_name (
+                id INT NOT NULL AUTO_INCREMENT,
+                name VARCHAR(255) NOT NULL,
+                city VARCHAR(255) NOT NULL,
+                sale BOOLEAN,
+                buy BOOLEAN,
+                phoneNumber VARCHAR(20),
+                phoneNumberWhatsapp VARCHAR(20),
+                address TEXT,
+                PRIMARY KEY (id)
+        ");
 	}
 
 }
